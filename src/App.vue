25fef2bbf0bc92aa0bea38352fcb4fb2fa7fbbd3
Mustafa Vatandaş <mustafavatandas@outlook.com>
2026-04-13 15:03:31 +0300

    Add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navbar from './components/layout/Navbar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
+    <Navbar />
+    <main class="container mx-auto px-4 py-8">
+      <RouterView />
+    </main>
+  </div>
+</template>
